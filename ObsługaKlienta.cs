099118f82c5c16ec40fe913f31bcc19ebd3acf61
Dj <justyndamaz@gmail.com>
2021-07-19 16:44:58 +0200

    filename change

--- ObsługaKlienta.cs
+++ ObsługaKlienta.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObsługaSerwera
+public class ObsługaKlienta
 {
     public static void PowitanieOtrzymane(int _odKlienta, Pakiet _pakiet)
     {
